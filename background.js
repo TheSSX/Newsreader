b5e85c113809bcee55f9445de878e4d71d354b92
Sam Glendenning <sam.glendenning@btinternet.com>
2019-10-21 19:27:46 +0100

    Hacky solution for removing <aside>

--- background.js
+++ background.js
@@ -33,6 +33,10 @@ function callSport()
                 randomlink = "https://www.theguardian.com/sport/" + links[Math.floor(Math.random()*links.length)];
             }
 
+            const publication = new SpeechSynthesisUtterance("The Guardian");
+            const topic = new SpeechSynthesisUtterance("Sport");
+            window.speechSynthesis.speak(publication);
+            window.speechSynthesis.speak(topic);
             extractArticle(randomlink);
         },
         error: function(error)
@@ -46,22 +50,34 @@ function extractArticle(theurl)
 {
     $.ajax({ url: theurl, success: function(data)
         {
-            let headline = data.split('<h1 class="content__headline " itemprop="headline">')[1].split('</h1>')[0];
-            if (headline === undefined)
-            {
-                headline = data.split('<span class="content__headline--interview-wrapper">')[1].split('</span>')[0];
-            }
+            let headline = data.split('<title>')[1].split('|')[0];
             console.log(headline);
+            const headlinemsg = new SpeechSynthesisUtterance(headline);
+            window.speechSynthesis.speak(headlinemsg);
 
             data = data.replace(/<figcaption [^|]+<\/figcaption>/g, '');
-            data = data.replace(/<aside [^|]+<\/aside>/g, '');
             data = data.replace(/<figure [^|]+<\/figure>/g, '');
-            data = data.replace(/<div data-twitter-event[^|]+<\/div>/g, '');
+            data = data.replace(/<twitter-widget [^|]+<\/twitter-widget>/g, '');
+
+            /**
+            This has been the bane of my life. Most other HTML elements can seemingly be removed
+            but <aside> can't. I have tried all I can to remove this and its containing text
+            from the data and the result is below. THIS NEEDS IMPROVING
+            */
+            //data = data.replace(/<aside [^|]+<\/aside>/g, '');
+            //data = data.replace('<aside .*?</aside>', '');
+
+            for (let i=0; i<3; i++)
+            {
+                data = data.split('<aside ')[0] + data.split('</aside>')[1];
+            }
 
             let articletext = $("<p>").html(data).text();
             articletext = articletext.split('Share via Email')[1];
             articletext = articletext.replace(/\r?\n|\r/g, "");
-            articletext = articletext.split('.').join('. ');
+            articletext = articletext.split('undefined')[0];
+            articletext = articletext.split('Topics')[0];
+            articletext = articletext.split('.').join('. ');        //has issues with decimal numnbers
 
             console.log(articletext);
             const msg = new SpeechSynthesisUtterance(articletext);
