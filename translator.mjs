53f93f233d9ccb397836b06d8781197ae9cee48b
CriTicalSSX <sam.glendenning@btinternet.com>
2019-11-15 11:55:10 +0000

    Translates articles and reads them in dialect of that country

---
+++ translator.mjs
@@ -0,0 +1,49 @@
+import {yandexurl, apikey} from "./yandex.js";
+
+/**
+ Class for translating articles via the Yandex free translation service.
+ Yandex offers one million free characters translated every day.
+ */
+export class Translator
+{
+    /**
+     * Manages the translation of text to a target language
+     * @param text - the text to translate
+     * @param targetlang - the target language to translate to
+     * @returns {Promise<undefined>} - the JSON response from Yandex
+     */
+    static async translate(text, targetlang)
+    {
+        const url = this.constructyandexurl(text, targetlang);
+
+        try
+        {
+            return await this.contactyandex(url);
+        }
+        catch
+        {
+            return undefined;
+        }
+    }
+
+    /**
+     * Constructs the GET request for querying the Yandex API
+     * @param text - the text to translate
+     * @param targetlang - the target language to translate to
+     * @returns {string} - the constructed URL
+     */
+    static constructyandexurl(text, targetlang)
+    {
+        return `${yandexurl}?key=${apikey}&text=${text}&lang=${targetlang}&options=1`;
+    }
+
+    /**
+     * Queries Yandex and returns JSON data
+     * @param url - the URL to query
+     * @returns {*} - the JSON data
+     */
+    static contactyandex(url)
+    {
+        return $.ajax({ url: url}).done(function(data){}).fail(function(ajaxError){});      //same here
+    }
+}
