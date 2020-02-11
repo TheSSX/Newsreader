3c60b5a234bf6eec6bfe5f45cdd1846e40f8e6ec
CriTicalSSX <sam.glendenning@btinternet.com>
2020-02-11 15:33:53 +0000

    Removed a line of code for testing

--- bulletin.mjs
+++ bulletin.mjs
@@ -38,8 +38,7 @@ export class Bulletin
 
     static retryTopic(topic, attempt)
     {
-        //const source = Object.keys(sources)[Math.floor(Math.random()*Object.keys(sources).length)];  //get random source to contact
-        const source = "The Independent";
+        const source = Object.keys(sources)[Math.floor(Math.random()*Object.keys(sources).length)];  //get random source to contact
 
         const topiclink = topics[topic][source];
         const data = PageParser.getArticle(source, topic, topiclink, sentences);          //send source, topic and number of sentences to summarise to
